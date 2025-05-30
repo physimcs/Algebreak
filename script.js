0b2aa9af5689ea66803e740a83e5c05bd70cd8d5
Sonia Tyburczy <styburczy.business@gmail.com>
2025-05-30 14:01:21 -0400

    Added toGame to move from loading screen to instructions page

---
+++ script.js
@@ -0,0 +1,7 @@
+function toGame() {
+    const loadingScreen = document.getElementById("loading-screen");
+    loadingScreen.remove();
+        
+    const instructions = document.getElementById("instructions");
+    instructions.style.display = "flex";
+}
